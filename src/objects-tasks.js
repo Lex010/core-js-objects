8eb60f42bfe76db182e45460f251c49a0eaa6e36
Lex Mel <lex07672@gmail.com>
2024-02-21 13:00:08 +0200

    feat: 08

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -160,8 +160,33 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  const bills = { 25: 0, 50: 0 };
+
+  for (let i = 0; i < queue.length; i += 1) {
+    const bill = queue[i];
+
+    if (bill === 25) {
+      bills[25] += 1;
+    } else if (bill === 50) {
+      if (bills[25] === 0) {
+        return false;
+      }
+      bills[25] -= 1;
+      bills[50] += 1;
+    } else if (bill === 100) {
+      if (bills[50] > 0 && bills[25] > 0) {
+        bills[50] -= 1;
+        bills[25] -= 1;
+      } else if (bills[25] >= 3) {
+        bills[25] -= 3;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
 
 /**
